26c23c75ab17a7f478eb2fa5d674f293a086209d
ShivuSG <45454397+ShivuSG@users.noreply.github.com>
2018-12-18 17:35:14 +0530

    Update sample.rb

--- sample.rb
+++ sample.rb
@@ -1,2 +1,3 @@
  price=("payoffAmount=53177.59,residualCB=13111.5,customerCBPrice=0.0,grossPayoff=53177.59,residualProcReturnedPrices=Y")
 puts value=price.split("payoffAmount=")[1].split(",")[0].to_s
+testing123
